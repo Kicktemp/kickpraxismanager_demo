c2740424464f7fa410d70852a9f23da64eb4d0a1
Niels Nübel <niels@kicktemp.com>
2022-07-27 16:40:29 +0200

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,61 @@
 <template>
   <div class="uk-container">
     <form class="uk-form-stacked uk-margin-large">
-      <div class="uk-margin" :class="{'uk-background-primary': location != ''}">
-        <label class="uk-form-label" for="form-horizontal-select">Standort wählen</label>
+      <div
+        class="uk-margin"
+        :class="{ 'uk-background-primary': location != '' }"
+      >
+        <label class="uk-form-label" for="form-horizontal-select"
+          >Standort wählen</label
+        >
         <div class="uk-form-controls">
           <div class="uk-flex-middle" uk-grid>
             <div class="uk-width-5-6">
-              <select class="uk-select" id="form-horizontal-select" v-model="location">
+              <select
+                class="uk-select"
+                id="form-horizontal-select"
+                v-model="location"
+              >
                 <option value="">Standort wählen</option>
                 <option
-                    v-for="location in locations"
-                    :key="location.attributes.locationid"
-                    :value="location.attributes.locationid"
+                  v-for="location in locations"
+                  :key="location.attributes.locationid"
+                  :value="location.attributes.locationid"
                 >
                   {{ location.attributes.title }}
                 </option>
               </select>
             </div>
             <div class="uk-width-1-6 uk-text-right">
-              <span v-if="location != ''" uk-icon="icon: check; ratio: 2"></span>
+              <span
+                v-if="location != ''"
+                uk-icon="icon: check; ratio: 2"
+              ></span>
             </div>
           </div>
         </div>
       </div>
       <div class="uk-margin" v-if="location != ''">
-        <label class="uk-form-label" for="interest_in">Select</label>
+        <label class="uk-form-label">Select</label>
         <div class="uk-form-controls">
-          <select class="uk-select" id="interest_in">
-            <option>Behandlung wählen</option>
+          <select class="uk-select" v-model="group">
+            <option value="">Bitte wählen</option>
             <option
-                v-for="interest in interests"
-                :key="interest.attributes.interestid"
-                :value="interest.attributes.interestid"
+              v-for="group in groups"
+              :key="group.attributes.id"
+              :value="group.attributes.id"
             >
-              {{ interest.attributes.title }} ({{ interest.attributes.category_title }})
+              {{ group.attributes.title }}
+            </option>
+          </select>
+          <select class="uk-select" v-model="interest" v-if="group != ''">
+            <option value="">Bitte wählen</option>
+            <option
+              v-for="interest in interests"
+              :key="interest.attributes.interestid"
+              :value="interest.attributes.interestid"
+            >
+              {{ interest.attributes.title }}
             </option>
           </select>
         </div>
@@ -59,9 +81,11 @@ export default {
     return {
       location: "",
       interest: "",
+      group: "",
       loading: false,
       locations: null,
       interests: null,
+      groups: null,
     };
   },
 
@@ -71,9 +95,12 @@ export default {
   },
 
   watch: {
-    location(newLocation, oldLocation) {
-        this.fetchInterests();
-    }
+    location(newLocation) {
+      if (newLocation !== "") this.fetchGroups();
+    },
+    group(newGroup) {
+      if (newGroup !== "") this.fetchInterests();
+    },
   },
 
   methods: {
@@ -86,47 +113,76 @@ export default {
       this.loading = true;
       const url = `${process.env.VUE_APP_API_URL}/${process.env.VUE_APP_BASE_PATH}/kickpraxismanager/locations`;
 
-      fetch(url, {headers: {"X-Joomla-Token": `${process.env.VUE_APP_JOOMLA_TOKEN}`}})
-          .then(async response => {
-            const data = await response.json();
+      fetch(url, {
+        headers: { "X-Joomla-Token": `${process.env.VUE_APP_JOOMLA_TOKEN}` },
+      })
+        .then(async (response) => {
+          const data = await response.json();
 
-            // check for error response
-            if (!response.ok) {
-              // get error message from body or default to response statusText
-              const error = (data && data.message) || response.statusText;
-              return Promise.reject(error);
-            }
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+          }
 
-            this.locations = data.data;
-            this.loading = false;
-          })
-          .catch(error => {
-            this.errorMessage = error;
-            console.error("There was an error!", error);
-          });
+          this.locations = data.data;
+          this.loading = false;
+        })
+        .catch((error) => {
+          this.errorMessage = error;
+          console.error("There was an error!", error);
+        });
+    },
+    async fetchGroups() {
+      this.loading = true;
+      const url = `${process.env.VUE_APP_API_URL}/${process.env.VUE_APP_BASE_PATH}/kickpraxismanager/groups/${this.location}`;
+
+      fetch(url, {
+        headers: { "X-Joomla-Token": `${process.env.VUE_APP_JOOMLA_TOKEN}` },
+      })
+        .then(async (response) => {
+          const data = await response.json();
+
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+          }
+
+          this.groups = data.data;
+          this.loading = false;
+        })
+        .catch((error) => {
+          this.errorMessage = error;
+          console.error("There was an error!", error);
+        });
     },
     async fetchInterests() {
       this.loading = true;
-      const url = `${process.env.VUE_APP_API_URL}/${process.env.VUE_APP_BASE_PATH}/kickpraxismanager/interests?location_id=${this.location}`;
+      const url = `${process.env.VUE_APP_API_URL}/${process.env.VUE_APP_BASE_PATH}/kickpraxismanager/interests/${this.location}/${this.group}`;
 
-      fetch(url, {headers: {"X-Joomla-Token": `${process.env.VUE_APP_JOOMLA_TOKEN}`}})
-          .then(async response => {
-            const data = await response.json();
+      fetch(url, {
+        headers: { "X-Joomla-Token": `${process.env.VUE_APP_JOOMLA_TOKEN}` },
+      })
+        .then(async (response) => {
+          const data = await response.json();
 
-            // check for error response
-            if (!response.ok) {
-              // get error message from body or default to response statusText
-              const error = (data && data.message) || response.statusText;
-              return Promise.reject(error);
-            }
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+          }
 
-            this.interests = data.data;
-            this.loading = false;
-          })
-          .catch(error => {
-            this.errorMessage = error;
-            console.error("There was an error!", error);
-          });
+          this.interests = data.data;
+          this.loading = false;
+        })
+        .catch((error) => {
+          this.errorMessage = error;
+          console.error("There was an error!", error);
+        });
     },
   },
 };
