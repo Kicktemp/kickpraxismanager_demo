9e75829fddb1aec58dea82af467f6eb96f2f139b
Niels Nübel <niels@kicktemp.com>
2022-08-10 15:11:12 +0200

    fix Resources

--- src/components/ResourcesSelect.vue
+++ src/components/ResourcesSelect.vue
@@ -6,6 +6,10 @@ export default {
       type: Number,
       default: 0,
     },
+    appointment: {
+      type: Number,
+      default: 0,
+    },
     interest: {
       type: Number,
       default: 0,
@@ -30,6 +34,7 @@ export default {
       resources: null,
       showDoctors: false,
       showNext: false,
+      resourceType: "",
     };
   },
   created() {
@@ -39,6 +44,27 @@ export default {
     interest(newInterest, oldInterest) {
       if (newInterest != oldInterest && newInterest != 0) {
         this.fetchResources();
+        this.$emit("update:showCalendar", false);
+        this.showDoctors = false;
+        this.resourceType = "";
+      }
+    },
+    appointment(newAppointment, oldAppointment) {
+      if (newAppointment != oldAppointment) {
+        this.$emit("update:showCalendar", false);
+        this.showDoctors = false;
+        this.resourceType = "";
+      }
+    },
+    resourceType(newResource) {
+      if (newResource == "early") {
+        this.$emit("update:showCalendar", true);
+        this.$emit("update:resource", 0);
+        this.showDoctors = false;
+      } else if (newResource == "doctors") {
+        this.$emit("update:showCalendar", false);
+        this.$emit("update:resource", "");
+        this.showDoctors = true;
       }
     },
   },
@@ -66,21 +92,22 @@ export default {
 
           this.resources = data.data;
 
-          if (this.resources[0].attributes.weightresource) {
-            this.$emit(
-              "update:weightlocation",
-              this.resources[0].attributes.weightresource
-            );
-            this.$emit("update:resource", this.resources[0].attributes.id);
+          if (this.resources[0] === undefined) {
             this.$emit("update:showCalendar", true);
-          }
+            this.$emit("update:resource", 0);
+          } else {
+            if (this.resources[0].attributes.weightresource) {
+              this.$emit(
+                "update:weightlocation",
+                this.resources[0].attributes.weightresource
+              );
+              this.$emit("update:resource", this.resources[0].attributes.id);
+              this.$emit("update:showCalendar", true);
+            }
 
-          if (this.resources[0].attributes.doctor_id) {
-            this.showNext = true;
-          }
-
-          if (this.resources[0] !== undefined) {
-            this.$emit("update:showCalendar", true);
+            if (this.resources[0].attributes.doctor_id) {
+              this.showNext = true;
+            }
           }
 
           // haben wir keine Behandler oder
@@ -97,7 +124,7 @@ export default {
 </script>
 
 <template>
-  <div class="uk-margin" v-if="showNext">
+  <div class="uk-margin uk-animation-slide-bottom-small" v-if="showNext">
     <label class="uk-form-label" for="behandler-praeferenz"
       >Sollen wir Ihnen einen frühstmöglichen Termin ohne Behandlerpräferenz
       anbieten oder möchten Sie eine Beratung bei einem Behandler Ihrer
@@ -109,9 +136,9 @@ export default {
           <div class="uk-grid">
             <div class="uk-width-1-2">
               <button
-                @mousedown="$emit('update:showCalendar', true)"
+                @mousedown="resourceType = 'early'"
                 class="uk-button"
-                :class="{ 'uk-button-primary': showCalendar }"
+                :class="{ 'uk-button-primary': this.resourceType == 'early' }"
                 type="button"
               >
                 Frühstmöglichen Termin auswählen
@@ -119,9 +146,9 @@ export default {
             </div>
             <div class="uk-width-1-2">
               <button
-                @click="showDoctors = true"
+                @click="resourceType = 'doctors'"
                 class="uk-button"
-                :class="{ 'uk-button-primary': showDoctors }"
+                :class="{ 'uk-button-primary': this.resourceType == 'doctors' }"
                 type="button"
               >
                 Behandler auswählen
@@ -130,12 +157,16 @@ export default {
           </div>
         </div>
         <div class="uk-width-1-6 uk-text-right">
-          <span v-if="customer != ''" uk-icon="icon: check; ratio: 2"></span>
+          <span
+            v-if="customer != ''"
+            uk-icon="icon: check; ratio: 2"
+            class="uk-text-success"
+          ></span>
         </div>
       </div>
     </div>
   </div>
-  <div class="uk-margin" v-if="showDoctors">
+  <div class="uk-margin uk-animation-slide-bottom-small" v-if="showDoctors">
     <label class="uk-form-label"
       >Bitte wählen Sie Ihren gewünschten Behandler aus</label
     >
@@ -145,7 +176,13 @@ export default {
         :key="resource.attributes.id"
         :value="resource.attributes.id"
       >
-        <div class="uk-card uk-card-default">
+        <div
+          class="uk-card uk-card-default uk-card-hover"
+          @mousedown="
+            $emit('update:showCalendar', true);
+            $emit('update:resource', resource.attributes.id);
+          "
+        >
           <div class="uk-card-media-top">
             <img
               :src="resource.attributes.data.image"
@@ -155,34 +192,6 @@ export default {
           <div class="uk-card-body">
             <h3 class="uk-card-title">{{ resource.attributes.data.title }}</h3>
           </div>
-        </div>
-      </div>
-    </div>
-    <div class="uk-form-controls">
-      <div class="uk-flex-middle" uk-grid>
-        <div class="uk-width-5-6">
-          <select
-            class="uk-select"
-            id="behandler"
-            v-model="resourceSelected"
-            @input="$emit('update:resource', $event.target.value)"
-          >
-            <option value="0">Bitte wählen</option>
-            <option
-              v-for="resource in resources"
-              :key="resource.attributes.id"
-              :value="resource.attributes.id"
-            >
-              {{ resource.attributes.name }}
-            </option>
-          </select>
-        </div>
-        <div class="uk-width-1-6 uk-text-right">
-          <span
-            v-if="resource != 0"
-            uk-icon="icon: check; ratio: 2"
-            class="uk-text-success"
-          ></span>
         </div>
       </div>
     </div>
